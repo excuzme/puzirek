c562190001fee09f2920d06b5f2ce2e6f83e0f48
Dmitriy <excuzme@yandex.ru>
2013-06-06 01:24:04 +0400

    1
    asdas

---
+++ puzirek/puzirek.h
@@ -0,0 +1,13 @@
+// puzirek.h
+
+#pragma once
+
+using namespace System;
+
+namespace puzirek {
+
+	public ref class Class1
+	{
+		// TODO: здесь следует добавить свои методы для этого класса.
+	};
+}
